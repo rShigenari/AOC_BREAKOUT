cbfe7cdbdbdc5f977acc71c1d3c1bcf0ddfc85a9
robl-25 <marianerobl@opmbx.org>
2016-11-01 21:08:48 -0200

    Adicionado a passagem de parametros para as funcoes

--- view/interface.asm
+++ view/interface.asm
@@ -9,9 +9,29 @@ Inicializa:
     li $k1, 266                     # Posicao inicial da bolinha em y
 
     li $v1, 0                       # Detecta se o jogo já começou
+    
+    addi $sp, $sp, -12
+    li $t8, 0x00FFFFFF
+    sw   $t8, 0($sp)
+    li $t8, 280
+    sw   $t8, 4($sp)
+    li $t8, 50
+    sw   $t8, 8($sp)
 
-    jal Bola                        # Desenha a bola
-    jal Barra                       # Desenha a barra
+    jal Barra                       # Desenha a bola
+    
+    addi $sp, $sp, -12
+    li $t8, 0x00FFFFFF
+    sw   $t8, 0($sp)
+    li $t8, 70
+    sw   $t8, 4($sp)
+    li $t8, 266
+    sw   $t8, 8($sp)
+    
+    jal Bola                       # Desenha a barra
+    
+    
+    
     j loop9                         # Inicia o jogo
 
 ######Desenhando varios retangulos#####
@@ -113,9 +133,15 @@ DrawPixel:
 ######Desenhando a barra#########
 Barra:
 
-    li   $a2, 0x00FFFFFF             # Carregando a cor branca para o registrador a2
-    addi $s1, $t6, 0                 # y1 = y posicao final da barra
-    addi $s0, $t7, 0                 # x1 = x posicao inicial da barra
+    #li   $a2, 0x00FFFFFF             # Carregando a cor branca para o registrador a2
+    #addi $s1, $t6, 0                 # y1 = y posicao final da barra
+    #addi $s0, $t7, 0                 # x1 = x posicao inicial da barra
+    lw $a2, 0($sp)
+    lw $s1, 4($sp)
+    lw $s0, 8($sp)    
+    addi $sp, $sp, 12
+    
+    move $t8, $s0
  
     addi $t2, $s0, 50                # Limite de x para pintar a barra
     addi $t1, $s1, 5                 # limite de y para pintar a barra
@@ -132,7 +158,7 @@ loop5:
 loop6:
 
     
-    move $s0, $t7                    # Reseta o x para o inicio
+    move $s0, $t8                    # Reseta o x para o inicio
     
     blt $s1, $t1, DrawPixel3         # Enquanto nao chegou no limite em y continue desenhando
     jr  $ra                          # Se chegou termine o programa
@@ -155,9 +181,15 @@ DrawPixel3:
 #####Desenhando a bolinha######
 Bola:
 
-    li   $a2, 0x00FFFFFF             # Carregando a cor branca para o registrador a2
-    move   $s1, $k1                  # y1 = y posicao inicial de y para desenhar a "bolinha"
-    move   $s0, $k0                  # x1 = x posicao inicial de x para desenhar a "bolinha"
+    #li   $a2, 0x00FFFFFF            # Carregando a cor branca para o registrador a2
+    #move   $s1, $k1                 # y1 = y posicao inicial de y para desenhar a "bolinha"
+    #move   $s0, $k0                 # x1 = x posicao inicial de x para desenhar a "bolinha"
+    lw $a2, 0($sp)
+    lw $s0, 4($sp)
+    lw $s1, 8($sp)
+    
+    addi $sp, $sp, 12
+    move $t8, $s0
  
     addi $t2, $s0, 14                # Posicao final de x
     addi $t1, $s1, 12                # Posicao final de y
@@ -174,7 +206,7 @@ loop7:
 loop8:
 
     addi $s1, $s1, 1                 # Pula para a proxima linha
-    move $s0, $k0                    # Reseta o x para o inicio
+    move $s0, $t8                    # Reseta o x para o inicio
     
     blt $s1, $t1, DrawPixel4         # Enquanto $s1 nao chegou no limite em y continue desenhando
     jr  $ra                          # Se chegou va para o loop do jogo
@@ -192,94 +224,22 @@ DrawPixel4:
     sw    $a2, ($t0)                 # Coloca a cor branca ($a2) em $t0
 
     j loop7                          # Volta para o loop de desenho
-
-#############Pinta a barra de preto########
+    
+#############Define como pra pintar a barra de Preto#########
 PintaPreto:
+    addi $sp, $sp, -12
+    li $k0, 0x00000000
+    sw   $k0, 0($sp)
+    sw   $t6, 4($sp)
+    sw   $t7, 8($sp)
+    
+    jal Barra
+    j DetectaMovimento
 
-    li   $a2, 0x00000000             # Carregando a cor preta para o registrador a2
-    addi $s1, $t6, 0                 # y1 = y posicao final da barra
-    addi $s0, $t7, 0                 # x1 = x posicao inicial da barra
- 
-    addi $t2, $s0, 50                # Limite de x para pintar a barra
-    addi $t1, $s1, 5                 # limite de y para pintar a barra
-    
-    j  loop10                         # Comece a desenhar
-    
- 
-loop10:
-
-   blt $s0, $t2, DrawPixel5          # Enquanto o inicio nao atingiu o limite (100) pinta os pixels em (s0, s1)
-   addi $s1, $s1, 1                  # Pula para a proxima linha
-   
-loop11:
-
-    
-    move $s0, $t7                    # Reseta o x para o inicio
-    
-    blt $s1, $t1, DrawPixel5         # Enquanto nao chegou no limite em y continue desenhando
-    j   DetectaMovimento             # Se chegou va ver para qual lado deve-se mover a barra
- 
- 
-DrawPixel5:
- 
-    addi  $s0, $s0, 1                # Adiciona 1 em x inicial
-    li    $t3, 0x10000100            # t3 = primeiro pixel da tela
- 
-    sll   $t0, $s1, 9                # y = y * 512
-    addu  $t0, $t0, $s0              # (xy) t0 = x + y
-    sll   $t0, $t0, 2                # (xy) t0 = xy * 4
-    addu  $t0, $t3, $t0              # Adiciona xy ao primeiro pixel ( t3 )
-    sw    $a2, ($t0)                 # Coloca a cor branca ($a2) em $t0
-
-    j     loop10                     # Volta para o loop de desenho
-    
-    
-#####Pinta a bolinha de preto######
-PintaPretoBola:
-
-    li     $a2, 0x00000000           # Carregando a cor preta para o registrador a2
-    move   $s1, $k1                  # y1 = y posicao inicial de y para desenhar a "bolinha"
-    move   $s0, $k0                  # x1 = x posicao inicial de x para desenhar a "bolinha"
- 
-    addi $t2, $s0, 14                # Posicao final de x
-    addi $t1, $s1, 12                # Posicao final de y
-    
-    j    loop12                      # Comece a desenhar
-    
- 
-loop12:
-
-   blt $s0, $t2, DrawPixel6          # Enquanto $s0 nao atingiu o limite (em x) pinta os pixels em (s0, s3)
-   j   loop13                        # Finaliza o programa
-   
-   
-loop13:
-
-    addi $s1, $s1, 1                 # Pula para a proxima linha
-    move $s0, $k0                    # Reseta o x para o inicio
-    
-    blt $s1, $t1, DrawPixel6         # Enquanto $s1 nao chegou no limite em y continue desenhando
-    j   MoverBola                    # Se chegou va para o loop do jogo
- 
- 
-DrawPixel6:
- 
-    addi  $s0, $s0, 1                # Adiciona 1 em x inicial
-    li    $t3, 0x10000100            # t3 = primeiro pixel da tela
- 
-    sll   $t0, $s1, 9                # y = y * 512
-    addu  $t0, $t0, $s0              # (xy) t0 = x + y
-    sll   $t0, $t0, 2                # (xy) t0 = xy * 4
-    addu  $t0, $t3, $t0              # Adiciona xy ao primeiro pixel ( t3 )
-    sw    $a2, ($t0)                 # Coloca a cor branca ($a2) em $t0
-
-    j loop12                         # Volta para o loop de desenho
-    
-    
 #############Detecta a entrada###########
 DetectaEntrada:
     
-    beq $a3, ' ', DetectaInicio      # Enquanto $a3 for igual a 'espaço' mova a bolinha
+    #beq $a3, ' ', DetectaInicio      # Enquanto $a3 for igual a 'espaço' mova a bolinha
     beq $a3, 'a', PintaPreto         # Se for 'a' eh para mover a barra
     beq $a3, 'd', PintaPreto         # Se for 'd' tambem move a barra
     j   loop9                        # Se nao for nem 'a', 'd' ou 'espaço' va para o loop
@@ -295,6 +255,7 @@ DetectaMovimento:
 MoverBola:
     subi $k1, $k1, 5                 # Move a bolinha em 1 em y
     
+    
     jal Bola                         # Move pra funcao de pintar a bolinha de novo na tela
     j loop9
     
@@ -302,6 +263,12 @@ MoverBola:
 MoverEsquerda:
     
     subi $t7, $t7, 7                 # Adiciona 1 na posicao em y da barra
+    
+    addi $sp, $sp, -12
+    li $k0, 0x00FFFFFF
+    sw   $k0, 0($sp)
+    sw   $t6, 4($sp)
+    sw   $t7, 8($sp)
     
     jal Barra
     j loop9
@@ -311,6 +278,12 @@ MoverEsquerda:
 MoverDireita:
     
     addi $t7, $t7, 7                 # Adiciona 1 na posicao em y da barra
+    
+    addi $sp, $sp, -12
+    li $k0, 0x00FFFFFF
+    sw   $k0, 0($sp)
+    sw   $t6, 4($sp)
+    sw   $t7, 8($sp)
     
     jal Barra
     j loop9
@@ -323,7 +296,7 @@ DetectaInicio:
     
 SetaInicio:
     li $v1, 1                       # Seta a variavel de controle para joga iniciado
-    j PintaPretoBola                # Va pintar a bolinha
+    #j PintaPretoBola                # Va pintar a bolinha
 
 #############Loop do jogo##########
 loop9:
